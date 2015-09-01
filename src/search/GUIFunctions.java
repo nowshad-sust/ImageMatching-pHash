135de578fb89cfcc98e7709ec2723e9625b8494b
nowshad_sust <iamnowshad@gmail.com>
2015-09-02 02:24:07 +0600

    refresh

---
+++ src/search/GUIFunctions.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package search;
+
+import java.awt.Button;
+
+/**
+ *
+ * @author nowshad
+ */
+public class GUIFunctions {
+    public void browseButtonClicked(Button bt){
+        browse();
+        getSelectedPath();
+    }
+
+    private void browse() {
+        
+    }
+
+    private String getSelectedPath() {
+        return null;
+    }
+}
